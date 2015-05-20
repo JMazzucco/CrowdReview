fd6b9864eee9724284c65305c658bbedf915e284
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-20 17:58:46 -0400

    Added Simple Password Authentication via Sorcery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_filter :require_login, only: [:index, :new, :create]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -13,4 +22,9 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :avatar)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
   resources :comments
-  resources :users
   resources :articles, only: [:show, :new, :create, :index]
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
 # do
 #     resources :comments, only: [:show, :create, :destroy]
