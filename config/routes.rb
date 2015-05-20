740210da277a561705162af4966fde3c0121821d
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-20 15:13:12 -0400

    added the plos gem and generated articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :articles, only: [:show, :new, :create, :index]
+
+# do
+#     resources :comments, only: [:show, :create, :destroy]
+#   endj
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
