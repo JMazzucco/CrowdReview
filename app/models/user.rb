a2a92fd2f85bd893aeb211ff51a50e3305b1f95a
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-21 07:50:46 -0400

    Added model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   validates :email, uniqueness: true
+
+  has_many :comments
+  has_many :articles, through: :comments
 end
