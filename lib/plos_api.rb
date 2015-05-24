a0fe5c5fb85e541cc1890665a65264851bb7bd7a
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-24 16:41:42 -0400

    remove debugging aids and example code

--- lib/plos_api.rb
+++ lib/plos_api.rb
@@ -3,20 +3,15 @@ class PlosApi
   require 'plos'
 
   def initialize
-    puts "PlosApi::initialize"
     @client = PLOS::Client.new(ENV["API_KEY"])
   end
 
   def get_articles
-    puts "PlosApi::get_articles"
     searchterm = 'apple'
     results = @client.search(searchterm, 50)
     results.each do |r|
-
-    # articles = @client.get_articles....
-    #                                articles.each do |article|
-    #   Article.create(name: article[:key])
-    # end
+      Article.create(title: r.title, abstract: r.abstract )
+    end
   end
 
 end
