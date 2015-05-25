35664ca3ca9c145ddeae1b70e8220328e96b7cc7
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-25 18:01:34 -0400

    Setting default value of votes attribute to zero

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
 	belongs_to :article
   belongs_to :user
   acts_as_tree order: 'created_at DESC'
+  after_initialize :init
+
+  private
+    def init
+        self.votes ||= 0
+    end
 end
