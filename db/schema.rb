2f44637876ca3ee364e2af964c2edf8931993229
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-05-26 15:38:36 -0400

    schema file changed after git pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525183941) do
+ActiveRecord::Schema.define(version: 20150526190649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20150525183941) do
     t.integer  "user_id"
     t.string   "title"
     t.integer  "parent_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
