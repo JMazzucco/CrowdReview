a8cafb3c4febce0d8d1784bd09dc9c76c0bda969
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-31 20:50:57 -0400

    custom rake take pulls articles from PLOS, removes the articles that have missing data, then adds them to the db.

--- lib/plos_api.rb
+++ lib/plos_api.rb
@@ -6,29 +6,26 @@ class PlosApi
     @client = PLOS::Client.new(ENV["API_KEY"])
   end
 
-  def collect_all_articles
+  def collect_all_articles(start)
     #a loop that will create an array of 10 new records from PLOS.
+    results = []
+     until results.count >= 10
+     @start = start.to_i
+     results = @client.all(@start, 100)
+     #if a record has less than 10 words in the abstract, remove it from the array of search results
 
-    # results = []
-    # until results.count => 10
-    #  results = @client.search(1, 10)
-    #  #if a record has less than 10 words in the abstract, remove it from the array of search results
-    #   results.each do |r|
-    #    if r.title && r.abstract.length > 10 && r.authors && r.id && r.published_at
-    #     results.delete(r)
-    #    end
-    #   end
-    #   binding.pry
-    # end
+      results.keep_if do |r|
+       !r.abstract.nil?
+      end
+      @start += 100
+     end
 
     results.each do |r|
       unless Article.find_by(plos_id: r.id)
-         if ( r.title && r.abstract.length > 10 && r.authors && r.id && r.published_at )
           Article.create(title: r.title, abstract: r.abstract[0],
                        publication_date: r.published_at,
                        authors: r.authors.to_sentence,
                        plos_id: r.id )
-         end
       end
     end
   end
--- lib/tasks/plos_fetch.rake
+++ lib/tasks/plos_fetch.rake
@@ -1,10 +1,8 @@
-
-
 namespace :plos_fetch do
   desc "Get articles from PLOS"
-  task collect_articles: :environment do
+ 	task :collect_articles, [:start_num] do |t, arg|
     api = PlosApi.new
-    api.collect_all_articles
-    puts "Added 10 new articles to the database"
+    api.collect_all_articles(arg.start_num)
+    puts "New articles were added to the database"
   end
 end
