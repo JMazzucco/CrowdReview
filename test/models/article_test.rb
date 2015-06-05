897000c26fe864ee2f2d7cbc79619f1d3fb5c11f
Tom Billingsley <tom.billingsley@gmail.com>
2015-06-05 11:18:59 -0400

    add positive test for custom validation

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -10,8 +10,13 @@ class ArticleTest < ActiveSupport::TestCase
     refute  copy.save, "saved a duplicate"
   end
 
-  test "validates abstract length" do
+  test "validatation reject incomplete absract" do
     inadequate = build(:boundary_case_one_short)
     refute  inadequate.save, "rejecting a nine word abstract"
+  end
+
+  test "validatation accept valid absract" do
+    adequate = build(:boundary_case_one_past)
+    assert  adequate.save, "rejecting a nine word abstract"
   end
 end
