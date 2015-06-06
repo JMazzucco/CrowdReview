14b4df2c821914e6921323c9e8395e57eb9423d3
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-06-05 20:13:34 -0400

    ran migration after master pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602123215) do
+ActiveRecord::Schema.define(version: 20150602194817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,15 +37,14 @@ ActiveRecord::Schema.define(version: 20150602123215) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "article_id"
     t.integer  "user_id"
     t.string   "title"
     t.integer  "parent_id"
     t.integer  "votes_count"
-    t.boolean  "flagged"
-    t.integer  "vote_test"
+    t.boolean  "flagged",     default: false
   end
 
   create_table "favorites", force: :cascade do |t|
