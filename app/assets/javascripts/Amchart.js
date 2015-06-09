775713852b3d05daa4e95bdc502c0bbe0f2b6d14
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-06-09 04:28:09 -0400

    inserted new trendline graph,modified coloring and text of other graphs while removed ones which were being tested out but not going to be used

--- app/assets/javascripts/Amchart.js
+++ app/assets/javascripts/Amchart.js
@@ -110,52 +110,28 @@ var chart = AmCharts.makeChart("chartdiv4", {
   "marginRight": 70,
   "path": "http://www.amcharts.com/lib/3/",
   "dataProvider": [{
-    "country": "USA",
-    "visits": 3025,
-    "color": "#FF0F00"
-  }, {
-    "country": "China",
-    "visits": 1882,
-    "color": "#FF6600"
-  }, {
-    "country": "Japan",
-    "visits": 1809,
-    "color": "#FF9E01"
-  }, {
-    "country": "Germany",
-    "visits": 1322,
-    "color": "#FCD202"
-  }, {
-    "country": "UK",
-    "visits": 1122,
-    "color": "#F8FF01"
-  }, {
-    "country": "France",
-    "visits": 1114,
-    "color": "#B0DE09"
-  }, {
-    "country": "India",
-    "visits": 984,
+    "country": "cited_by_twitter_count",
+    "visits": 200,
     "color": "#04D215"
   }, {
-    "country": "Spain",
-    "visits": 711,
+    "country": "cited_by_facebook_count",
+    "visits": 150,
     "color": "#0D8ECF"
   }, {
-    "country": "Netherlands",
-    "visits": 665,
+    "country": "cited_by_google+_count",
+    "visits": 110,
     "color": "#0D52D1"
   }, {
-    "country": "Russia",
-    "visits": 580,
+    "country": "cited_by_blogposts_count",
+    "visits": 75,
     "color": "#2A0CD0"
   }, {
-    "country": "South Korea",
-    "visits": 443,
+    "country": "cited_by_linkedin_count",
+    "visits": 50,
     "color": "#8A0CCF"
   }, {
-    "country": "Canada",
-    "visits": 441,
+    "country": "cited_by_wikipedia_count",
+    "visits": 35,
     "color": "#CD0D74"
   }],
   "valueAxes": [{
@@ -184,97 +160,54 @@ var chart = AmCharts.makeChart("chartdiv4", {
   },
   "export": {
     "enabled": true
-  }
-});
-
-/**
- * Second chart
- */
-var chart = AmCharts.makeChart("chartdiv2", {
-  "theme": "light",
-  "type": "serial",
-  "dataProvider": [{
-    "country": "USA",
-    "year2004": 3.5,
-    "year2005": 4.2
-  }, {
-    "country": "UK",
-    "year2004": 1.7,
-    "year2005": 3.1
-  }, {
-    "country": "Canada",
-    "year2004": 2.8,
-    "year2005": 2.9
-  }, {
-    "country": "Japan",
-    "year2004": 2.6,
-    "year2005": 2.3
-  }, {
-    "country": "France",
-    "year2004": 1.4,
-    "year2005": 2.1
-  }, {
-    "country": "Brazil",
-    "year2004": 2.6,
-    "year2005": 4.9
-  }],
-  "startDuration": 1,
-  "graphs": [{
-    "fillAlphas": 0.9,
-    "lineAlpha": 0.2,
-    "title": "2004",
-    "type": "column",
-    "valueField": "year2004"
-  }, {
-    "fillAlphas": 0.9,
-    "lineAlpha": 0.2,
-    "title": "2005",
-    "type": "column",
-    "clustered": false,
-    "columnWidth": 0.5,
-    "valueField": "year2005"
-  }],
-  "plotAreaFillAlphas": 0.1,
-  "categoryField": "country",
+  },
   "titles": [{
     "text": "Second chart"
   }, {
     "text": "Scroll down for the next chart",
     "bold": false
   }]
-
 });
 
 /**
- * Third chart
+ * NEW Third chart
  */
-var chart = AmCharts.makeChart("chartdiv3", {
+
+var chart = AmCharts.makeChart("chartdiv5", {
   "type": "serial",
+  "addClassNames": true,
   "theme": "light",
+  "path": "http://www.amcharts.com/lib/3/",
+  "autoMargins": false,
+  "marginLeft": 60,
+  "marginRight": 40,
+  "marginTop": 70,
+  "marginBottom": 60,
+  "balloon": {
+    "adjustBorderColor": false,
+    "horizontalPadding": 10,
+    "verticalPadding": 8,
+    "color": "#ffffff"
+  },
+
   "dataProvider": [{
-    "year": 2009,
-    "income": 23.5,
-    "expenses": 21.1
+    "year": "One Week",
+    "score": 172
   }, {
-    "year": 2010,
-    "income": 26.2,
-    "expenses": 30.5
+    "year": "One Month",
+    "score": 203
   }, {
-    "year": 2011,
-    "income": 30.1,
-    "expenses": 34.9
+    "year": "Three Months",
+    "score": 279
   }, {
-    "year": 2012,
-    "income": 29.5,
-    "expenses": 31.1
+    "year": "Six Months",
+    "score": 1057
   }, {
-    "year": 2013,
-    "income": 30.6,
-    "expenses": 28.2,
+    "year": "One Year",
+    "score": 1212
   }, {
-    "year": 2014,
-    "income": 34.1,
-    "expenses": 32.9,
+    "year": "Current",
+    "score": 5160,
     "dashLengthColumn": 5,
     "alpha": 0.2,
     "additional": "(projection)"
@@ -286,6 +219,7 @@ var chart = AmCharts.makeChart("chartdiv3", {
   "startDuration": 1,
   "graphs": [{
     "alphaField": "alpha",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
     "fillAlphas": 1,
     "title": "Income",
     "type": "column",
@@ -293,6 +227,7 @@ var chart = AmCharts.makeChart("chartdiv3", {
     "dashLengthField": "dashLengthColumn"
   }, {
     "id": "graph2",
+    "balloonText": "<span style='font-size:12px;'>[[title]] in [[category]]:<br><span style='font-size:20px;'>[[value]]</span> [[additional]]</span>",
     "bullet": "round",
     "lineThickness": 3,
     "bulletSize": 7,
@@ -302,8 +237,8 @@ var chart = AmCharts.makeChart("chartdiv3", {
     "bulletBorderThickness": 3,
     "fillAlphas": 0,
     "lineAlpha": 1,
-    "title": "Expenses",
-    "valueField": "expenses"
+    "title": "Score",
+    "valueField": "score"
   }],
   "categoryField": "year",
   "categoryAxis": {
@@ -311,12 +246,16 @@ var chart = AmCharts.makeChart("chartdiv3", {
     "axisAlpha": 0,
     "tickLength": 0
   },
+  "export": {
+    "enabled": true
+  },
   "titles": [{
     "text": "Third chart"
   }, {
     "text": "Scroll down for the next chart",
     "bold": false
   }]
+
 });
 
 })
